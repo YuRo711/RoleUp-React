0238368fb2fe3621df98a26458c5abbe5d4317d6
YuRo711 <rohlinay@mail.ru>
2024-07-19 14:22:36 +0300

    header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,19 @@
+import Header from '../Header/Header';
+import { LanguageContext } from "../../contexts/LanguageContext";
 import './App.css';
+import { useState } from 'react';
+import { texts } from '../../utils/texts';
 
 function App() {
+  const [lang, setLang] = useState("ru");
+
   return (
     <div className="page">
+      <LanguageContext.Provider value={{lang, setLang}}>
+        <Header
+          texts={texts[lang].header}
+        />
+      </LanguageContext.Provider>
     </div>
   );
 }
